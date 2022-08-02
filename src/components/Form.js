8ef51674ee807b2b69f7a3cb1c0f3e6c1d792bf6
Achdiat Hadit <59441372+achdiathadit@users.noreply.github.com>
2022-08-02 16:27:19 +0700

    Restructure folder

---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+const Form = () => {
+	return (
+		<form autoComplete='off'>
+			<label htmlFor='name'>Name</label>
+			<input id='name' type='text' placeholder='Enter your name' />
+		</form>
+	);
+};
+export default Form;
