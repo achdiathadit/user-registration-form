a18a5417b910050de405f7293215e726ffab2132
Achdiat Hadit <59441372+achdiathadit@users.noreply.github.com>
2022-08-03 10:11:11 +0700

    Update form

--- src/components/RegisForm.js
+++ src/components/RegisForm.js
@@ -40,88 +40,145 @@ const RegisForm = () => {
 	return (
 		<div className='card'>
 			<form onSubmit={handleSubmit} autoComplete='off'>
-				<label htmlFor='name'>Name</label>
-				<input
-					value={values.name}
-					onChange={handleChange}
-					id='name'
-					type='text'
-					placeholder='Enter your name'
-					className={errors.name && touched.name ? 'input-error' : ''}
-				/>
-				{errors.name && touched.name && <p className='error'>{errors.name}</p>}
-				<label htmlFor='dateOfBirth'>Date of Birth</label>
-				<DatePicker
-					selected={values.dateOfBirth}
-					dateFormat='MMMM d, yyyy'
-					name='dateOfBirth'
-					onChange={(date) => setFieldValue('dateOfBirth', date)}
-					className={
-						errors.dateOfBirth && touched.dateOfBirth ? 'input-error' : ''
-					}
-					showMonthDropdown
-					showYearDropdown
-					dropdownMode='select'
-				/>
-				{errors.placeOfBirth && touched.placeOfBirth && (
-					<p className='error'>{errors.placeOfBirth}</p>
-				)}
-				<label htmlFor='placeOfBirth'>Place of Birth</label>
-				<input
-					value={values.placeOfBirth}
-					onChange={handleChange}
-					id='placeOfBirth'
-					type='text'
-					placeholder='Enter your place of birth'
-					className={
-						errors.placeOfBirth && touched.placeOfBirth ? 'input-error' : ''
-					}
-				/>
-				{errors.placeOfBirth && touched.placeOfBirth && (
-					<p className='error'>{errors.placeOfBirth}</p>
-				)}
-				<label htmlFor='address'>Address</label>
-				<input
-					value={values.address}
-					onChange={handleChange}
-					id='address'
-					type='text'
-					placeholder='Enter your address'
-					className={errors.address && touched.address ? 'input-error' : ''}
-				/>
-				{errors.address && touched.address && (
-					<p className='error'>{errors.address}</p>
-				)}
-				<label htmlFor='phoneNumber'>Phone Number</label>
-				<input
-					value={values.phoneNumber}
-					onChange={handleChange}
-					id='phoneNumber'
-					type='tel'
-					placeholder='Enter your phone number'
-					className={
-						errors.phoneNumber && touched.phoneNumber ? 'input-error' : ''
-					}
-				/>
-				{errors.phoneNumber && touched.phoneNumber && (
-					<p className='error'>{errors.phoneNumber}</p>
-				)}
-				<label htmlFor='email'>Email Address</label>
-				<input
-					value={values.email}
-					onChange={handleChange}
-					id='email'
-					type='email'
-					placeholder='Enter your email address'
-					className={errors.email && touched.email ? 'input-error' : ''}
-				/>
-				{errors.email && touched.email && (
-					<p className='error'>{errors.email}</p>
-				)}
-				<button type='submit' disabled={isSubmitting}>
-					Submit
-				</button>
-				<button onClick={handleReset}>Reset</button>
+				<div className='form-container'>
+					<fieldset className='left-fieldset'>
+						<label htmlFor='name'>Name</label>
+						<input
+							value={values.name}
+							onChange={handleChange}
+							id='name'
+							type='text'
+							placeholder='Enter your name'
+							className={errors.name && touched.name ? 'input-error' : ''}
+						/>
+						{errors.name && touched.name && (
+							<p className='error'>{errors.name}</p>
+						)}
+						<label htmlFor='dateOfBirth'>Date of Birth</label>
+						<DatePicker
+							selected={values.dateOfBirth}
+							dateFormat='MMMM d, yyyy'
+							name='dateOfBirth'
+							onChange={(date) => setFieldValue('dateOfBirth', date)}
+							className={
+								errors.dateOfBirth && touched.dateOfBirth ? 'input-error' : ''
+							}
+							showMonthDropdown
+							showYearDropdown
+							dropdownMode='select'
+						/>
+						{errors.placeOfBirth && touched.placeOfBirth && (
+							<p className='error'>{errors.placeOfBirth}</p>
+						)}
+						<label htmlFor='placeOfBirth'>Place of Birth</label>
+						<input
+							value={values.placeOfBirth}
+							onChange={handleChange}
+							id='placeOfBirth'
+							type='text'
+							placeholder='Enter your place of birth'
+							className={
+								errors.placeOfBirth && touched.placeOfBirth ? 'input-error' : ''
+							}
+						/>
+						{errors.placeOfBirth && touched.placeOfBirth && (
+							<p className='error'>{errors.placeOfBirth}</p>
+						)}
+						<label htmlFor='address'>Address</label>
+						<input
+							value={values.address}
+							onChange={handleChange}
+							id='address'
+							type='text'
+							placeholder='Enter your address'
+							className={errors.address && touched.address ? 'input-error' : ''}
+						/>
+						{errors.address && touched.address && (
+							<p className='error'>{errors.address}</p>
+						)}
+						<label htmlFor='phoneNumber'>Phone Number</label>
+						<input
+							value={values.phoneNumber}
+							onChange={handleChange}
+							id='phoneNumber'
+							type='tel'
+							placeholder='Enter your phone number'
+							className={
+								errors.phoneNumber && touched.phoneNumber ? 'input-error' : ''
+							}
+						/>
+						{errors.phoneNumber && touched.phoneNumber && (
+							<p className='error'>{errors.phoneNumber}</p>
+						)}
+						<label htmlFor='email'>Email Address</label>
+						<input
+							value={values.email}
+							onChange={handleChange}
+							id='email'
+							type='email'
+							placeholder='Enter your email address'
+							className={errors.email && touched.email ? 'input-error' : ''}
+						/>
+						{errors.email && touched.email && (
+							<p className='error'>{errors.email}</p>
+						)}
+					</fieldset>
+					<fieldset className='right-fieldset'>
+						<label htmlFor='title'>Title</label>
+						<select
+							value={values.title}
+							onChange={handleChange}
+							id='title'
+							className={errors.title && touched.title ? 'input-error' : ''}
+						>
+							<option value='null'>Please select your title</option>
+							<option value='mr'>Mr.</option>
+							<option value='mrs'>Ms.</option>
+						</select>
+						<label htmlFor='marriageStatus'>Marriage Status</label>
+						<select
+							value={values.marriageStatus}
+							onChange={handleChange}
+							id='marriageStatus'
+							className={
+								errors.marriageStatus && touched.marriageStatus
+									? 'input-error'
+									: ''
+							}
+						>
+							<option value='null'>Please select your marriage status</option>
+							<option value='single'>Single</option>
+							<option value='married'>Married</option>
+							<option value='divorced'>Divorced</option>
+						</select>
+						{errors.marriageStatus && touched.marriageStatus && (
+							<p className='error'>{errors.marriageStatus}</p>
+						)}
+						<label htmlFor='uploadedImages'>Images</label>
+						<input
+							value={values.uploadedImages}
+							onChange={handleChange}
+							id='uploadedImages'
+							type='file'
+							multiple
+							accept='image/png, image/jpeg'
+							className={
+								errors.uploadedImages && touched.uploadedImages
+									? 'input-error'
+									: 'file-upload-button'
+							}
+						/>
+						{errors.uploadedImages && touched.uploadedImages && (
+							<p className='error'>{errors.uploadedImages}</p>
+						)}
+					</fieldset>
+				</div>
+				<div className='button-container'>
+					<button type='submit' disabled={isSubmitting}>
+						Submit
+					</button>
+					<button onClick={handleReset}>Reset</button>
+				</div>
 			</form>
 		</div>
 	);
